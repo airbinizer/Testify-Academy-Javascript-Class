237b25eaec99832e668a784885eb01dcc052315c
airbinizer <airbiniza94@gmail.com>
2026-08-26 00:13:54 +0100

    Task 11

---
+++ Tasks/Task11.js
@@ -0,0 +1,9 @@
+const books = {
+    title: "Making it Big",
+    description: "A book about success, ambition, business, and achieving big goals.",
+    numberOfPages: 300,
+    authour: "Donald Trump",
+    reading: true
+};
+
+console.log(books);
