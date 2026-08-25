c1e695d4c8ea73cea639e82d9837ec883a46d124
airbinizer <airbiniza94@gmail.com>
2026-08-26 00:02:40 +0100

    Task 8

---
+++ Tasks/Task8.js
@@ -0,0 +1,4 @@
+const myArray = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friiday', 'Saturday', 'Sunday']
+const numArray = [100, 78, 83]
+const emptyArray = []
+console.log[emptyArray.length]
