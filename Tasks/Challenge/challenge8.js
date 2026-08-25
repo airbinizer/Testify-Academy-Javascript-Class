419ff35654a7131de70e7c7c665de7bb2c93f2c3
airbinizer <airbiniza94@gmail.com>
2026-08-26 00:54:03 +0100

    challenge 8

---
+++ Tasks/Challenge/challenge8.js
@@ -0,0 +1,7 @@
+//Return a Boolean if a number is divisible by 10
+function divisibleBy10(number) {
+    return number % 10 === 0;
+}
+
+console.log(divisibleBy10(50));
+console.log(divisibleBy10(53));
