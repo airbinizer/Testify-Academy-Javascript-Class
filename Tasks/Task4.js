e809444c90d8ec0f7185de837f76e4b938f7e516
airbinizer <airbiniza94@gmail.com>
2026-08-29 14:29:53 +0100

    Task 4

--- Tasks/Task4.js
+++ Tasks/Task4.js
@@ -1,11 +1,1 @@
-const side1 = 5;
-const side2 = 5;
-const side3 = 3;
-
-if (side1 === side2 && side2 === side3) {
-    console.log("Equilateral triangle");
-} else if (side1 === side2 || side1 === side3 || side2 === side3) {
-    console.log("Isosceles triangle");
-} else {
-    console.log("Scalene triangle");
-}
+alert("Hello, world!");
